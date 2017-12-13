174625ba0561c85bfb08e4b7ad8d11aa3e88ac87
PACS Student <interjo@hotmail.it>
2017-12-13 16:55:42 +0100

    Restructured repository

---
+++ TreeRoot_Time_solver/src/3_Initial_P0/main.cpp
@@ -0,0 +1,21 @@
+#include <root_problem3d1d.hpp>
+
+using namespace getfem;
+
+
+int main(int argc, char * argv[]){
+ 	root_problem3d1d prob;
+ 	prob.init(argc, argv);
+ 	prob.assembly();
+	prob.solve();
+	prob.export_vtk();
+
+
+	std::cout << "--- FINAL RESULTS -------------------------" << std::endl; 
+	std::cout << "  Pt average            = " << prob.mean_pt()   << std::endl;
+	std::cout << "  Pv average            = " << prob.mean_pv()   << std::endl;
+	std::cout << "  Uv average            = " << prob.mean_uv()   << std::endl;
+	std::cout << "  Network-to-Tissue TFR = " << prob.flow_rate() << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
+	//prob.Uplot();
+}
